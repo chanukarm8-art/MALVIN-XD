ad4bcb0a315b4de7265e985c27ef1fa832ccc2c3
chanukarm8-art <chanukarm8@gmail.com>
2025-09-08 21:10:29 +0530

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~VnEBnSyT#dHnrCdGjDkFHJuluZD64nqUmTW_Z9s33tUGm1xreMZg",  // add your Session Id here
   
   OWNER_NUMBER: "263714757857", // put your phone number here
   
