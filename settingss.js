e90919b7e1aef11572d8b74ad007ade30b4500f9
chanukarm8-art <chanukarm8@gmail.com>
2025-09-08 21:09:42 +0530

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,10 +1,10 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~VnEBnSyT#dHnrCdGjDkFHJuluZD64nqUmTW_Z9s33tUGm1xreMZg",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "94705330441", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
-  TIMEZONE: "Africa/Harare" //put your country timeZone....leave blank if u don't know.
+  TIMEZONE: "Srilanka/colombo" //put your country timeZone....leave blank if u don't know.
 };
